73f78b170bdc4a514f73f870efd797ca81414bcb
TheIcarus- <34997295+LBSimab@users.noreply.github.com>
2025-09-18 00:02:32 +0330

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const joi = require("joi");
 
 const User = require("../models/users");
-joi.object({
+const createuserschema = joi.object({
   name: joi.string().min(3).required(),
   email: joi.string().email().required(),
   password: joi.string().min(6).required(),
@@ -12,6 +12,14 @@ joi.object({
 });
 router.post("/", async (req, res) => {
   const { name, email, password, adress } = req.body;
+  //JoI VALIDATION
+
+  const joinvalidation = createuserschema.validate(req.body);
+
+  if (joinvalidation.error) {
+    return res.status(400).json(joinvalidation.error.details[0].message);
+  }
+
   const user = await User.findOne({ email: email });
 
   if (user) {
