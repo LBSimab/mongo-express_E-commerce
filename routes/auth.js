56c657b2ef53d94aebf27ed2a788e1840cf0e01b
TheIcarus- <34997295+LBSimab@users.noreply.github.com>
2025-09-23 19:45:22 +0330

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,21 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 
-app.get(
+router.get(
   "/google",
   passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "http://localhost:5173/login",
+  }),
+  (req, res) => {
+    const profile = req.user;
+    res.json(profile);
+  }
 );
 
 module.exports = router;
