8668a796331f23721f16fe607b98465e2777ca65
TheIcarus- <34997295+LBSimab@users.noreply.github.com>
2025-09-29 02:27:24 +0330

    cat

--- routes/category.js
+++ routes/category.js
@@ -1,4 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/category");
+const multer = require("multer");
 
+const upload = multer({
+  dest: "upload/category/",
+});
+
+router.post("/", upload.single("icon"), async (req, res) => {
+  if (!req.file || !req.body.name) {
+    return res.status(400).json({ message: "name and icon is required!" });
+  }
+  console.log(req.file);
+  const newCategory = new Category({
+    name: req.body.name,
+    image: req.file.filename,
+  });
+
+  await newCategory.save();
+
+  res.status(201).json({
+    message: "Category succesfully added!",
+    category: newCategory,
+  });
+});
 module.exports = router;
