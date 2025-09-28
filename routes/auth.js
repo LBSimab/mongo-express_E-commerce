808d363435b533be89ee514dbd9590e184486353
TheIcarus- <34997295+LBSimab@users.noreply.github.com>
2025-09-28 18:01:04 +0330

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
 
 router.get(
   "/google",
@@ -13,9 +15,36 @@ router.get(
     session: false,
     failureRedirect: "http://localhost:5173/login",
   }),
-  (req, res) => {
+  async (req, res) => {
+    //check user if it exist or not using googleId or email
     const profile = req.user;
-    res.json(profile);
+    let user = await User.findOne({
+      $or: [{ googleId: profile.id }, { email: profile.emails[0].value }],
+    });
+
+    //if user available - update the googleid and generate the token and send it to response
+    if (user) {
+      if (!user.googleId) {
+        user.googleId = profile.id;
+        await user.save();
+      }
+    }
+
+    //user is not available so we create user with googleid and generate the token and send it to response
+    else {
+      user = new User({
+        name: profile.displayName,
+        email: profile.emails[0].value,
+        googleId: profile.id,
+      });
+      await user.save();
+    }
+    const token = jwt.sign(
+      { _id: user._id, name: user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: "2h" }
+    );
+    res.redirect(`http://localhost:5173/dashboard?token=${token}`);
   }
 );
 
