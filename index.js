48f4878a85eae1c63c1a776eed114e5808f74e18
TheIcarus- <34997295+LBSimab@users.noreply.github.com>
2025-10-06 22:30:20 +0330

    logger and error handling middleware

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const userRoutes = require("./routes/users");
 const categoryRoutes = require("./routes/category");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
+const winston = require("winston");
+
 PORT = process.env.PORT || 5001;
 //database connection
 mongoose
@@ -17,12 +19,27 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+//lets create some logger with winston package
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({ level: "debug" }),
+    new winston.transports.File({
+      filename: "logs/errors.log",
+      level: "error",
+    }),
+  ],
+});
 //using built-in middleware for requests and responses
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 ///static files i.e images
 app.use("/upload/categories", express.static("upload/category"));
+
 app.use("/upload/products", express.static("upload/products"));
 //routes
 app.use("/api/users", userRoutes);
@@ -30,8 +47,20 @@ app.use("/api/products", productsRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/auth", authRoutes);
 
+//addin error logger middleware
+app.use((error, req, res, next) => {
+  console.log("error middleware is running ");
+  //log the error in file or database   // cheeky logger
+  logger.error(error.message, {
+    method: req.method,
+    path: req.originalUrl,
+    stack: error.stack,
+  });
+  return res.status(500).json({ message: "internal server error!" });
+});
+
 try {
-  app.listen(PORT, console.log("server is running on port:", PORT));
+  app.listen(PORT, logger.info("server is running on port:", PORT));
 } catch (error) {
   console.log(error);
 }
